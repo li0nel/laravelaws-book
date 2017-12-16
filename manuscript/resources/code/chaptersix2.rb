dcab96680950cb8f6f5c524e8ac840bf8454416d
sriniatritex <34539662+sriniatritex@users.noreply.github.com>
2017-12-16 07:31:26 -0800

    Add files via upload

---
+++ manuscript/resources/code/chaptersix2.rb
@@ -0,0 +1,12 @@
+   # Add your key to your SSH agent
+   ssh-add -K laravelaws.pem
+
+   # Verify that your private key is successfully loaded in your local SSH agent
+   ssh-add –L
+
+   # Use the -A option to enable forwarding of the authentication agent connection
+   ssh –A ec2-user@<bastion-public-IP-address>
+
+   # Once you are connected to the bastion, you can SSH into a private subnet instance
+   # without copying any SSH key on the bastion
+   ssh ec2-user@<instance-private-IP-address>
