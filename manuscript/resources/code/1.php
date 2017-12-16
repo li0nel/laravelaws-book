7760fbfd8bfe9eb34ba6036f4498789b25a818e8
sriniatritex <34539662+sriniatritex@users.noreply.github.com>
2017-12-16 01:38:49 -0800

    Add files via upload

---
+++ manuscript/resources/code/1.php
@@ -0,0 +1,18 @@
+<?php
+ echo "hello World"
+
+ foreach ($variable as $key => $value) {
+ # code...
+ }
+
+ /**
+ * Hello World Class
+ */
+ class HelloWorld extends AnotherClass
+ {
+
+ function __construct(argument)
+ {
+ # code...
+ }
+ }
