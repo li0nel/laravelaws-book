9363041a3454103c53f056c4e5f6c349fbebd8bf
sriniatritex <34539662+sriniatritex@users.noreply.github.com>
2017-12-16 07:27:54 -0800

    Delete chaptersix1.rb

--- manuscript/resources/code/chaptersix1.rb
+++
@@ -1,8 +0,0 @@
-   aws ec2 run-instances
-       --image-id ami-c1a6bda2
-       --key-name laravelaws            # the SSH key pair we created earlier
-       --security-group-ids sg-xxxxxxxx # our previous SG allowing access to the DB
-       --subnet-id subnet-xxxxxxxx      # one of our public subnets
-       --count 1
-       --instance-type t2.micro         # the smallest instance type allowed
-       --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=bastion}]'
