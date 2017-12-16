db01e8b3f9b171e1c921e7519db178d49c0564ec
sriniatritex <34539662+sriniatritex@users.noreply.github.com>
2017-12-16 06:21:09 -0800

    Delete chapterfour1.rb

--- manuscript/resources/code/chapterfour1.rb
+++
@@ -1,7 +0,0 @@
-   |--- master.yaml         # the root template
-   |--- infrastructure          
-      |--- vpc.yaml            # our VPC and security groups
-      |--- storage.yaml     # our database cluster and S3 bucket
-      |--- web.yaml           # our ECS cluster
-      |--- services.yaml    # our ECS Tasks Definitions &amp; Services
-
