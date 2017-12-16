18bb59dce47a7058d00d229f253bd037ed7d0df9
sriniatritex <34539662+sriniatritex@users.noreply.github.com>
2017-12-16 06:24:50 -0800

    Delete chapternine3.rb

--- manuscript/resources/code/chapternine3.rb
+++
@@ -1,2 +0,0 @@
-   * * * * * root . /etc/cron.d/project_env.sh ; /usr/local/bin/php /var/www/html/artisan schedule:run &> /var/log/cron.log
-   # An empty line is required at the end of this file for a valid cron file.
