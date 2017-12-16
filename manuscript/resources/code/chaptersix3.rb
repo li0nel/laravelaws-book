efe0398bfecb00035d254c40f7cdb8904eadd039
sriniatritex <34539662+sriniatritex@users.noreply.github.com>
2017-12-16 07:23:11 -0800

    Add files via upload

---
+++ manuscript/resources/code/chaptersix3.rb
@@ -0,0 +1,3 @@
+   # Use the Docker exec command to execute the Artisan commands inside the application container
+   docker exec -it CONTAINER_ID php artisan session:table
+   docker exec -it CONTAINER_ID php artisan migrate?--force
