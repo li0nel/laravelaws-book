1facbf221e89a9cd6689bf2ec90b2ff68b275ab2
sriniatritex <34539662+sriniatritex@users.noreply.github.com>
2017-12-16 06:20:11 -0800

    Delete chaptereleven1.rb

--- manuscript/resources/code/chaptereleven1.rb
+++
@@ -1,8 +0,0 @@
-   DatabaseReplicaInstance:
-       Type: AWS::RDS::DBInstance
-       DependsOn: DatabasePrimaryInstance
-       Properties:
-           Engine: aurora
-           DBClusterIdentifier: !Ref DatabaseCluster
-           DBInstanceClass: !Ref DatabaseInstanceType
-           DBSubnetGroupName: !Ref DatabaseSubnetGroup
