632b15064ed2a985afdfb46106ba93bcb9683158
sriniatritex <34539662+sriniatritex@users.noreply.github.com>
2017-12-16 06:20:50 -0800

    Delete chapterfive4.rb

--- manuscript/resources/code/chapterfive4.rb
+++
@@ -1,7 +0,0 @@
-   # Building our Nginx Docker image and tagging it with the ECR URL
-   docker build -f Dockerfile-nginx -t YOUR_ECR_REGISTRY_URL_HERE:nginx .
-   docker push YOUR_ECR_REGISTRY_URL_HERE:nginx
-
-   # Building our Laravel Docker image and tagging it with the ECR URL
-   docker build -t YOUR_ECR_REGISTRY_URL_HERE:laravel .
-   docker push YOUR_ECR_REGISTRY_URL_HERE:laravel
