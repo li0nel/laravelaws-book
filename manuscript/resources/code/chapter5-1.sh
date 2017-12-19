3e2a8250a5aeb13fcdeccc0665956685b111a494
Lionel Martin <corsaaaire@gmail.com>
2017-12-19 13:44:21 +0000

    copy fixes

--- manuscript/resources/code/chapter5-1.sh
+++ manuscript/resources/code/chapter5-1.sh
@@ -1,4 +1,5 @@
-# The get-login command outputs the "docker login" command you need to execute with a temporary token
-# You can run both directly using eval
-# The --no-include-email tells get-login not to return the -e option that does not work for all of Docker versions
+# The get-login command outputs the "docker login" command you need to execute
+# with a temporary token. You can run both directly using eval.
+# The --no-include-email tells get-login not to return the -e option that does
+# not work for all of Docker versions
 eval $(aws ecr get-login --no-include-email)
